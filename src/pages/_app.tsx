6c643c8ea9729b890140c92c265fca12728add3e
LuisGazcon <luiselgazcon@gmail.com>
2021-08-26 15:51:18 -0700

    feat: Poppins and DM Serif Display fonts added to styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 
+import '@/resources/scss/index.scss';
+
 const App = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />;
 };
