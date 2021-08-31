9200f6746ddfd3844377f0bc2a8ded567ce5a918
LuisGazcon <luiselgazcon@gmail.com>
2021-08-30 18:12:04 -0700

    fix: fixed support for CSS Modules with camelCase exportLocalsConvention and addes sass path aliases

--- next.config.js
+++ next.config.js
@@ -1,5 +1,32 @@
-const webpack = require('./webpack.config');
+const path = require('path');
+const SassAlias = require('sass-alias');
 
 module.exports = {
-	webpack: webpack,
+	poweredByHeader: false,
+	reactStrictMode: true,
+	sassOptions: {
+		importer: new SassAlias({
+			'@resources': path.join(__dirname, 'src', 'resources'),
+		}),
+	},
+	webpack: (config) => {
+		config.module.rules[3].oneOf.forEach((moduleLoader, i) => {
+			Array.isArray(moduleLoader.use) &&
+				moduleLoader.use.forEach((l) => {
+					if (l.loader.includes('\\css-loader') && !l.loader.includes('postcss-loader')) {
+						const { getLocalIdent, ...others } = l.options.modules;
+
+						l.options = {
+							...l.options,
+							modules: {
+								...others,
+								localIdentName: '[hash:base64:8]',
+								exportLocalsConvention: 'camelCase',
+							},
+						};
+					}
+				});
+		});
+		return config;
+	},
 };
